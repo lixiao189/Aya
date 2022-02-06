1ae2c69c586d1434fe766e30442a80d1d5876277
Node Sans <node189@icloud.com>
2022-02-06 12:02:29 +0800

    注册页面大体完工

--- src/pages/RegisterPage.vue
+++ src/pages/RegisterPage.vue
@@ -1,3 +1,14 @@
+<script lang="ts" setup>
+import Logo from "../components/Logo.vue";
+import Footer from "../components/Footer.vue";
+import RegisterForm from "../components/register/RegisterForm.vue";
+</script>
+
 <template>
-<div>register</div>
+  <Logo />
+  <RegisterForm id="register-form" />
+  <Footer />
 </template>
+
+<style>
+</style>
