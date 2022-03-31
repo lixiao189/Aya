8029b54abed06e305440f539c3cc73797b45ee90
Node Sans <node189@icloud.com>
2022-04-01 00:17:41 +0800

    register page 删除了没有必要的封装

--- src/pages/RegisterPage.vue
+++ src/pages/RegisterPage.vue
@@ -1,14 +1,50 @@
 <script lang="ts" setup>
 import Logo from "../components/Logo.vue";
 import Footer from "../components/Footer.vue";
-import RegisterForm from "../components/register/RegisterForm.vue";
+
+import { NCard, NForm, NSpace, NInput, NButton, NFormItem } from "naive-ui";
 </script>
 
 <template>
   <Logo />
-  <RegisterForm id="register-form" />
+
+  <NSpace style="margin-top: 30px; margin-bottom: 55px" :justify="'center'">
+    <NCard title="用户注册" :size="'large'">
+      <NForm>
+        <NFormItem label="手机号">
+          <NInput placeholder="请输入你的手机号"></NInput>
+        </NFormItem>
+
+        <NFormItem label="身份证号">
+          <NInput placeholder="请输入你的身份证号"></NInput>
+        </NFormItem>
+
+        <NFormItem label="密码">
+          <NInput placeholder="请输入你的密码" :type="'password'"></NInput>
+        </NFormItem>
+
+        <NFormItem label="确认密码">
+          <NInput placeholder="请再次输入你的密码" :type="'password'"></NInput>
+        </NFormItem>
+      </NForm>
+
+      <div>
+        <NButton id="register-button" color="#ea0437">注册</NButton>
+      </div>
+    </NCard>
+  </NSpace>
+
   <Footer />
 </template>
 
 <style>
+.n-card {
+  min-width: 500px;
+}
+
+#register-button {
+  margin-top: 11px;
+  margin-bottom: 11px;
+  width: 100%;
+}
 </style>
