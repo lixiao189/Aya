1518862bd69d860391cb2ffb7196b189128a246b
slainsama <slainnico@gmail.com>
2022-03-31 22:15:41 +0800

    new ordercard

---
+++ src/components/order/OrderCard.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { NCard } from 'naive-ui';
+import { NButton } from 'naive-ui';
+import { NTag } from 'naive-ui';
+</script>
+
+<template>
+<n-card title="产品1" class="order-card">
+    <template #header-extra>
+      <NTag type="warning">状态</NTag>
+    </template>
+      这是个产品
+    <template #footer>
+      价格
+    </template>
+    <template #action>
+      <NButton class="order-button-1">支付</NButton>
+      <NButton class="order-button-2">取消</NButton>
+    </template>
+</n-card>
+</template>
+
+<style>
+.order-card{
+  width: 30vw;
+  margin-top: 5vh;
+  margin-left: 2vw;
+}
+</style>
---
+++ src/pages/OrderManager.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { NSpace } from "naive-ui"
+import logo from "../components/Logo.vue"
+import Footer from "../components/Footer.vue"
+import topbar from "../components/order/TopBar.vue"
+import ordercard from "../components/order/OrderCard.vue"
+</script>
+
+<template>
+  <logo/> 
+  <topbar/>
+  <NSpace id="back-space">
+    <ordercard/>
+    <ordercard/>
+    <ordercard/>
+    <ordercard/>
+    <ordercard/>
+    <ordercard/>
+  </NSpace>
+  <Footer/>
+</template>
+
+<style>
+#back-space{
+  height: auto;
+}
+</style>
