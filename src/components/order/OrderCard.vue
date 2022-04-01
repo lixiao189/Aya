550d3fab482b59a1b1ce6e50b6e0ed5e22f9de0f
slainsama <slainnico@gmail.com>
2022-04-01 10:24:37 +0800

    remake ordercard

--- src/components/order/OrderCard.vue
+++ src/components/order/OrderCard.vue
@@ -2,16 +2,24 @@
 import { NCard } from 'naive-ui';
 import { NButton } from 'naive-ui';
 import { NTag } from 'naive-ui';
+
+defineProps<{
+  create_time: string; //创建时间
+  status: string;  //订单状态
+  purchase_number: number; //购买份数
+  total_price: number; //订单总金额，单位为分
+}>();
 </script>
 
 <template>
 <n-card title="产品1" class="order-card">
     <template #header-extra>
-      <NTag type="warning">状态</NTag>
+      <NTag type="warning">{{ status }}</NTag>
     </template>
-      这是个产品
+      创建时间：{{ create_time }}
+      购买份数：{{ purchase_number }}
     <template #footer>
-      价格
+      价格：{{ total_price }}
     </template>
     <template #action>
       <NButton class="order-button-1">支付</NButton>
--- src/pages/OrderManager.vue
+++ src/pages/OrderManager.vue
@@ -4,19 +4,42 @@ import logo from "../components/Logo.vue"
 import Footer from "../components/Footer.vue"
 import topbar from "../components/order/TopBar.vue"
 import ordercard from "../components/order/OrderCard.vue"
+import { ref } from "vue"
+import { Order } from "../define/Order"
+import { NGrid,NGridItem } from "naive-ui"
+import { onMounted } from "vue"
+
+let orderlist = ref<Order[]>([]); // 调用临时数据
+
+onMounted(() => {
+  for (let i = 0; i < 10; i++) {
+    orderlist.value.push({
+      oid:"123456",
+      pid:"123456",
+      create_time:"yyyy-MM-dd HH:mm:ss",
+      status:"未支付",
+      purchase_number:3,
+      total_price:1000000,
+    });
+  }
+});
+
+
 </script>
 
 <template>
   <logo/> 
   <topbar/>
-  <NSpace id="back-space">
-    <ordercard/>
-    <ordercard/>
-    <ordercard/>
-    <ordercard/>
-    <ordercard/>
-    <ordercard/>
-  </NSpace>
+  <NGrid :cols="3">
+    <NGridItem v-for="order in orderlist">
+      <ordercard
+        :status="order['status']"
+        :create_time="order['create_time']"
+        :purchase_number="order['purchase_number']"
+        :total_price="order['total_price']"
+      />
+    </NGridItem>
+  </NGrid>
   <Footer/>
 </template>
 
