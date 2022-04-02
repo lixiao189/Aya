fd9293bf45f4a6d3ed71eff54d19356ed5052662
Node Sans <node189@icloud.com>
2022-04-02 12:25:03 +0800

    稍微整理了一下 Router.ts

--- src/config/Router.ts
+++ src/config/Router.ts
@@ -3,12 +3,17 @@ import { createRouter, createWebHistory } from "vue-router";
 export default createRouter({
   history: createWebHistory(),
   routes: [
+    // ===================== Market Page Router ===============================
     { path: "/", component: () => import("../pages/MarketIndex.vue") },
     { path: "/login", component: () => import("../pages/LoginPage.vue") },
     { path: "/register", component: () => import("../pages/RegisterPage.vue") },
+    { path: "/order", component: () => import("../pages/OrderManager.vue") },
+
+    // ===================== Admin Page Router ===============================
     {
       path: "/admin",
       component: () => import("../pages/AdminPage.vue"),
+
       children: [
         {
           path: "",
@@ -20,6 +25,5 @@ export default createRouter({
       path: "/admin/login",
       component: () => import("../pages/AdminLoginPage.vue"),
     },
-    { path: "/order", component: () => import("../pages/OrderManager.vue")}
   ],
 });
