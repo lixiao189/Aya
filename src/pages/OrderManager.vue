91071c849b9fa6125f1e676d3f0de4810c09fe64
Node Sans <node189@icloud.com>
2022-04-03 11:34:36 +0800

    Order 页面布局调整

--- src/pages/OrderManager.vue
+++ src/pages/OrderManager.vue
@@ -1,38 +1,37 @@
 <script lang="ts" setup>
-import { NSpace } from "naive-ui"
-import logo from "../components/Logo.vue"
-import Footer from "../components/Footer.vue"
-import topbar from "../components/order/TopBar.vue"
-import ordercard from "../components/order/OrderCard.vue"
-import { ref } from "vue"
-import { Order } from "../define/Order"
-import { NGrid,NGridItem } from "naive-ui"
-import { onMounted } from "vue"
+import { NSpace } from "naive-ui";
+import logo from "../components/Logo.vue";
+import Footer from "../components/Footer.vue";
+import topbar from "../components/TopBar.vue";
+import OrderCard from "../components/order/OrderCard.vue";
+import { ref } from "vue";
+import { Order } from "../define/Order";
+import { NGrid, NGridItem } from "naive-ui";
+import { onMounted } from "vue";
 
 let orderlist = ref<Order[]>([]); // 调用临时数据
 
 onMounted(() => {
   for (let i = 0; i < 10; i++) {
     orderlist.value.push({
-      oid:"123456",
-      pid:"123456",
-      create_time:"yyyy-MM-dd HH:mm:ss",
-      status:"未支付",
-      purchase_number:3,
-      total_price:1000000,
+      oid: "123456",
+      pid: "123456",
+      create_time: "yyyy-MM-dd HH:mm:ss",
+      status: "未支付",
+      purchase_number: 3,
+      total_price: 1000000,
     });
   }
 });
-
-
 </script>
 
 <template>
-  <logo/> 
-  <topbar/>
-  <NGrid :cols="3">
+  <logo />
+  <topbar />
+  <NGrid cols="4 1450:5">
     <NGridItem v-for="order in orderlist">
-      <ordercard
+      <OrderCard
+        class="order-card"
         :status="order['status']"
         :create_time="order['create_time']"
         :purchase_number="order['purchase_number']"
@@ -40,11 +39,15 @@ onMounted(() => {
       />
     </NGridItem>
   </NGrid>
-  <Footer/>
+  <Footer />
 </template>
 
 <style>
-#back-space{
+#back-space {
   height: auto;
+}
+
+.order-card {
+  width: 300px;
 }
 </style>
