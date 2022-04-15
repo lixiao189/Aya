1ca6c629604e167c57a1185f5b76c72961b105b6
Node Sans <node189@icloud.com>
2022-04-15 14:49:41 +0800

    根据是否是 admin 页面来决定是否要用自定义主题

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
 import Theme from "./config/Theme";
 
-import { RouterView } from "vue-router";
+import { RouterView, useRoute } from "vue-router";
 import { NDialogProvider, NConfigProvider, NMessageProvider } from "naive-ui";
+import { computed } from "@vue/reactivity";
+
+// 定义 computed 变量
+const smartTheme = computed(() => {
+  // 如果是 admin 页面就不使用自定义主题
+  const path = useRoute().path;
+
+  if (path.indexOf("/admin") != -1) return {};
+  else return Theme;
+});
 </script>
 
 <template>
-  <NConfigProvider :theme-overrides="Theme">
+  <NConfigProvider :theme-overrides="smartTheme">
     <NDialogProvider>
       <NMessageProvider>
         <RouterView />
